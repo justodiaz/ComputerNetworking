735441593761300a1fab18dd78797148681cfe9e
Justo Diaz Esquivel <jdiaze2@uic.edu>
2015-09-20 00:39:58 -0500

    created a Game class

--- hw3/server.cpp
+++ hw3/server.cpp
@@ -23,6 +23,11 @@
 #define LOSE 1
 #define DRAW 2
 
+#define NOPLAYER 0
+#define SENDWANT 1
+#define SENDCARD 2
+#define INBATTLE 3
+
 #define BACKLOG 100
 
 using namespace std;
@@ -99,81 +104,164 @@ int accept_player(int listener){
 	return newfd;
 }
 
-verify_request(char *command){
-		std::cout << "Player #"<<player_num<<" confirmed." << std::endl;
-	else
-		i_error("Player confirmation failed: Wrong protocol message.");
+int Shutdown(int sockfd, int how){
+	int rc;
+	rc = shutdown(sockfd,how);
+	if(rc < 0){
+		perror("Shutdown error");
+		exit(1);
+	}
+
+	return rc;
+}
+
+typedef struct {
+	int state;
+	int fd;
+	vector<char> deck;
+	int battle_card;
+} Player;
+
+
 
 class Game{
-	int state1; //verifying players or playing
-	int state2; //verifying players or playing
-	int fd1;
-	int fd2;
-	array<char, 52> deck;
-	vector<char> deck1;
-	vector<char> deck2;
+	Player player1;
+	Player player2;
+
+	void clear_fds(Player &player, Player &other){
+		cout << "Client on fd " << player.fd << " misbehaved." <<endl;
+		Shutdown(player.fd,SHUT_RDWR);
+		
+		if(other.state != NOPLAYER) Shutdown(other.fd,SHUT_RDWR);
+	}
+
+	bool valid_card(vector<char> &mydeck, char card){
+		auto iter = find(mydeck.begin(),mydeck.end(),card);
+		
+		if(iter == mydeck.end()) return false;
+		
+		mydeck.erase(iter);
+		return true;
+	}
+
+	void play_battle(){
+		player1.battle_card %= 13;
+		player2.battle_card %= 13;
+		
+		char result1[2] = {PLAYRESULT,0};
+		char result2[2] = {PLAYRESULT,0};
+		
+		if(player1.battle_card > player2.battle_card)
+			{ result1[1] = WIN; result2[1] = LOSE; }
+		else if (player1.battle_card < player2.battle_card)
+			{ result1[1] = LOSE; result2[1] = WIN; }
+		else
+			{ result1[1] = DRAW; result2[1] = DRAW; }
+		
+		Send(player1.fd,result1, sizeof result1,0);
+		Send(player2.fd,result2, sizeof result2,0);
+			
+	}
+
+	bool _resolve(Player &player,Player &other){
+		switch(player.state){
+		case SENDWANT:
+		{ //should send 'want' message
+			char want[2] = {0};
+			Recv(player.fd,want, sizeof want,0);
+
+			if(want[0] != WANTGAME || want[1] != 0){
+				clear_fds(player,other);
+				return false;
+			}
+
+			player.state = SENDCARD;
+
+			char gamestart[27];
+			gamestart[0] = GAMESTART;
+			for(int i=1;i<=26;i++) gamestart[i] = player.deck[i-1];
+			Send(player.fd,gamestart,sizeof gamestart,0);
+
+			break;
+		}
+		case SENDCARD:
+		{//playing, should send a card
+			char play[2] = {0};
+			Recv(player.fd,play,sizeof play,0);
+			if(play[0] != PLAYCARD || !valid_card(player.deck, play[1])){
+				clear_fds(player,other);
+				return false;
+			}
+
+			player.battle_card = (int)play[1];	
+
+			if(other.state == INBATTLE){
+				play_battle();
+
+				if(player.deck.empty() || other.deck.empty()) {
+					cout << "Game ended.";
+					Shutdown(player.fd,SHUT_RDWR);
+					Shutdown(other.fd,SHUT_RDWR);
+					return false;
+				}
+
+				player.state = other.state = SENDCARD;
+			}
+			else
+				player.state = INBATTLE;
+
+			break;	
+		}
+		case INBATTLE: //sending data after card already sent	
+			clear_fds(player,other);
+			break;
+
+		default: i_error("Game::Resolve invalid state");
+
+		}
+		
+		return true;
+	}
+
 	public:
-	Game(int start_fd){
-		fd1 = start_fd;
-		state1 = 1;
-		state2 = 0;
+	Game(int start_fd) : 
+	player1.state(SENDWANT), player1.fd(start_fd), player1.battle_card(-1),
+	player2.state(NOPLAYER), player2.fd(-1), player2.battle_card(-1)
+	{
+		array<char, 52> deck;
 		for(int i=0;i<52;i++) deck[i] = i;
 
-		srand( unsigned (time(0)) );
 		random_shuffle(deck.begin(), deck.end());
 		
 		for(int i=0;i<52;i++){
 			if(i<26)
-				deck1.push_back(deck[i]);	
+				player1.deck.push_back(deck[i]);	
 			else
-				deck2.push_back(deck[i]);
+				player2.deck.push_back(deck[i]);
 		}
 	}
 
-	bool iMember(int fd) {return fd1 == fd || fd2 == fd;}
+	bool iMember(int fd) {return player1.fd == fd || player2.fd == fd;}
 
 	bool addPlayer(int fd){
-		if(state2 != 0) return false;
+		if(player2.state != NOPLAYER) return false;
 
-		fd2 = fd;
-		state2 = 1;
+		player2.fd = fd;
+		player2.state = SENDWANT;
 	
 		return true;
 	}
 	
-	void resolve(int fd){
+	bool resolve(int fd){
 
-		if(fd == fd1){
-			switch(state1){
-				case 1:
-					char want[2] = {0};
-					Recv(fd,want, sizeof want,0);
-					if(want[0] == WANTGAME && want[1] == 0){
-						state1 = 2;
-						char gamestart[27];
-						gamestart[0] = GAMESTART;
-						 
-							
-					}
-					break;
-				case 2:
-
-					break;
-				default: i_error("Game::Resolve invalid state");
-
-			}		
-		}
-		else if(fd == fd2){
-
-
-
-		}
+		if(fd == player1.fd) return _resolve(player1,player2);
+		else if(fd == player2.fd) return _resolve(player2,player1);
 		else i_error("Game::Resolve received invalid fd"); 
 
 	}
 	
 
-}
+};
 
 int main(int argc, char **argv){
 
@@ -222,25 +310,27 @@ int main(int argc, char **argv){
 	
 	fd_set main, readfds;
 	
-	FD_SET(listener, main);
+	FD_SET(listener, &main);
 
-	vector<Games> games;
+	srand( unsigned (time(0)) );
+	vector<Game> games;
 	for(;;){
 		readfds = main;
-		select(maxfd+1, readfds, NULL, NULL, NULL);
+		select(maxfd+1, &readfds, NULL, NULL, NULL);
 		
-		for(int i=0;i<=maxfd;i++)
-			if(i == listener && FD_ISSET(listener, readfds)){//a client is trying to connect
+		for(int i=0;i<=maxfd;i++){
+			if(i == listener && FD_ISSET(listener, &readfds)){//a client is trying to connect
 				int newfd = accept_player(listener);
-				FD_SET(newfd, main);
+				if(newfd > maxfd) maxfd = newfd;
+				FD_SET(newfd, &main);
 			}
 			
-			if(FD_ISSET(i,readfds)){ //about to receive data
-					resolve(i);
+			if(FD_ISSET(i,&readfds)){ //about to receive data
+	//				resolve(i);
 				
 			}
 			
-			
+		}
 
 	}
 	
