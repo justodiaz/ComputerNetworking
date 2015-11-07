684f7ac3c4a8573aad9f21ad56d8f139d68b5dd7
Justo Diaz Esquivel <jdiaze2@uic.edu>
2015-11-06 22:44:10 -0600

    added acks, no timeouts yet

--- hw6/hw6.c
+++ hw6/hw6.c
@@ -34,15 +34,56 @@ int rel_rtt(int socket) {
 		 return 1000;
 }
 
+void make_pkt(int seq_num, char *data, int data_len, char *packet){
+	hdr_ptr pkt_hdr = (hdr_ptr)packet;
+	memset(packet,0,MAX_PACKET);	
+	pkt_hdr->sequence_number = htonl(seq_num);
+	pkt_hdr->ack_number = htonl(0);
+
+	memcpy(pkt_hdr+1,data,data_len);
+}
+
+int isACK(char *rcvpkt, int ack_num){
+	hdr_ptr hdr = (hdr_ptr)rcvpkt;
+	
+	return ntohl(hdr->ack_number) == ack_num;
+}
+
+//caller gurantees len will be no longer than MAX_SEGMENT
 void rel_send(int sock, void *buf, int len)
 {
  	// make the packet = header + buf
-	char packet[1400];
-	struct hw6_hdr *hdr = (struct hw6_hdr*)packet;
-	hdr->sequence_number = htonl(sequence_number);
-	memcpy(hdr+1,buf,len); //hdr+1 is where the payload starts
+	char packet[MAX_PACKET];
+	//hdr_ptr hdr = (hdr_ptr)packet;
+
+	//hdr->sequence_number = htonl(sequence_number); //Don't think htonl is necessary
+	//hdr->ack_number = htonl(0);
+
+	//memcpy(hdr+1,buf,len); //hdr+1 is where the payload starts
+	make_pkt(sequence_number,buf,len,packet);
+
+	char rcvpkt[MAX_PACKET];
+	do{
+		//udt_send(packet)
+		send(sock, packet,HDR_SZ+len, 0);
+		//await for ack
+		memset(rcvpkt,0,MAX_PACKET);
+		//start timer
+		//unsigned start = current_msec();
+		//alarm(TimeoutInterval);
+		//should timeout if doesn't recieve ack
+		//should block until ack is received
+		recv(sock, rcvpkt, MAX_PACKET, 0);
+		//if(ret<0){ if(errno == EINTR) timeout = true; else exit(1); }
+		//unsigned end = current_msec();
+		//unsigned SampleRTT = end - start;
+		//DevRTT = (1-.25) * DevRTT + .25 * abs(SampleRTT - EstimatedRTT);
+		//EstimatedRTT = (1-.125) * EstimatedRTT + .125 * SampleRTT;
+		//TimeoutInterval = EstimatedRTT + 4 * DevRTT;
+		
+	} while( /*sock timeout or */ !isACK(rcvpkt,sequence_number)); //resend
 	
-	send(sock, packet, sizeof(struct hw6_hdr)+len, 0);
+
 	sequence_number++;
 }
 
@@ -51,10 +92,24 @@ int rel_socket(int domain, int type, int protocol) {
 	return socket(domain, type, protocol);
 }
 
+int has_seq(char *rcvpkt, int seq_num){
+	hdr_ptr hdr = (hdr_ptr)rcvpkt;
+	
+	fprintf(stderr, "Got packet %d\n", ntohl(hdr->sequence_number));
+
+	return ntohl(hdr->sequence_number) == seq_num;	
+}
+
+void make_ack(char *sndpkt, int ack_num){
+	hdr_ptr hdr = (hdr_ptr)sndpkt;
+	hdr->ack_number = htonl(ack_num);
+	hdr->sequence_number = htonl(0);
+}
+
 int rel_recv(int sock, void * buffer, size_t length) {
 	char packet[MAX_PACKET];
 	memset(&packet,0,sizeof(packet));
-	struct hw6_hdr* hdr=(struct hw6_hdr*)packet;	
+//	hdr_ptr hdr=(hdr_ptr)packet;	
 
 	struct sockaddr_in fromaddr;
 	unsigned int addrlen=sizeof(fromaddr);	
@@ -66,10 +121,23 @@ int rel_recv(int sock, void * buffer, size_t length) {
 		perror("couldn't connect socket");
 	}
 
-	fprintf(stderr, "Got packet %d\n", ntohl(hdr->sequence_number));
+	char sndpkt[HDR_SZ];
 
-	memcpy(buffer, packet+sizeof(struct hw6_hdr), recv_count-sizeof(struct hw6_hdr));
-	return recv_count - sizeof(struct hw6_hdr);
+	while(!has_seq(packet,sequence_number)){
+		make_ack(sndpkt,sequence_number-1);
+		send(sock,sndpkt,HDR_SZ,0);
+ 
+		memset(&packet,0,sizeof(packet));
+		recv_count = recv(sock,packet,MAX_PACKET,0);
+	}	
+
+	make_ack(sndpkt,sequence_number);
+	send(sock,sndpkt,HDR_SZ,0);
+
+	sequence_number++;
+
+	memcpy(buffer, packet+HDR_SZ, recv_count-HDR_SZ);
+	return recv_count-HDR_SZ;
 }
 
 int rel_close(int sock) {
--- hw6/hw6.h
+++ hw6/hw6.h
@@ -7,6 +7,9 @@ struct hw6_hdr {
 
 #define MAX_PACKET 1400
 #define MAX_SEGMENT (MAX_PACKET-sizeof(struct hw6_hdr))
+#define HDR_SZ (sizeof(struct hw6_hdr))
+
+typedef struct hw6_hdr* hdr_ptr;
 
 int rel_socket(int domain, int type, int protocol);
 int rel_connect(int socket,struct sockaddr_in *toaddr,int addrsize);
