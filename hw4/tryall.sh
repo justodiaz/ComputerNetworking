fadf2022e1d00ddb32c3ef6d5275ef768107c481
Justo Diaz Esquivel <jdiaze2@uic.edu>
2015-12-20 12:07:50 -0600

    added grading stuff

---
+++ hw4/tryall.sh
@@ -0,0 +1,24 @@
+dig @localhost -p 5454 www.yahoo.com.tw
+dig @localhost -p 5454 www.yahoo.co.in
+
+dig @localhost -p 5454 www.yahoo.com
+
+dig @localhost -p 5454 www.microsoft.com
+dig @localhost -p 5454 www.apple.com
+dig @localhost -p 5454 www.linkedin.com
+dig @localhost -p 5454 www.facebook.com
+
+dig @localhost -p 5454 www.internic.net
+dig @localhost -p 5454 www.uic.edu
+
+dig @localhost -p 5454 www.cs.uic.edu
+dig @localhost -p 5454 nibbles.cs.uic.edu
+
+dig @localhost -p 5454 www.domain.invalid
+dig @localhost -p 5454 nonexistent.kaytwo.org
+
+dig @localhost -p 5454 www.thelongestdomainnameintheworldandthensomeandthensomemoreandmore.com
+
+dig @localhost -p 5454 ds.v6ns.test-ipv6.com
+
+dig @localhost -p 5454 ipv6.google.com 
---
+++ hw4/tryalltest.sh
@@ -0,0 +1,24 @@
+dig     www.yahoo.com.tw
+dig     www.yahoo.co.in
+
+dig     www.yahoo.com
+
+dig     www.microsoft.com
+dig     www.apple.com
+dig     www.linkedin.com
+dig     www.facebook.com
+
+dig     www.internic.net
+dig     www.uic.edu
+
+dig     www.cs.uic.edu
+dig     nibbles.cs.uic.edu
+
+dig     www.domain.invalid
+dig     nonexistent.kaytwo.org
+
+dig     www.thelongestdomainnameintheworldandthensomeandthensomemoreandmore.com
+
+dig     ds.v6ns.test-ipv6.com
+
+dig     ipv6.google.com 
